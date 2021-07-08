821faecce70f908a53926a2fdb7280101a640cc4
theevagaraju <1161203412@student.mmu.edu.my>
2021-07-08 23:25:54 +0800

    doctype created

--- firstapp/firstapp/doctype/firstapp_student/firstapp_student.py
+++ firstapp/firstapp/doctype/firstapp_student/firstapp_student.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+import frappe
 from frappe.model.document import Document
 
 class FirstappStudent(Document):
-	pass
+	def validate (self) : # keyword for default
+		total_student = len(frappe.get_all("Firstapp Student"))
+		total_tutor = len(frappe.get_all("Firstapp Tutor"))
+		total_course = len(frappe.get_all("Firstapp Course"))
+		frappe.db.set_value("Firstapp Setting",None,"total_student", total_student)
+		frappe.db.set_value("Firstapp Setting",None,"total_tutor", total_tutor)
+		frappe.db.set_value("Firstapp Setting",None,"total_course", total_course)
+		frappe.db.commit()
